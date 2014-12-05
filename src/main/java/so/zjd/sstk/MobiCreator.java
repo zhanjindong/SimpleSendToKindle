d74ff659ba0b986edf47687d57841a94b92fc4b2
zhanjindong <zhanjindong1989@qq.com>
2014-12-05 21:52:46 +0800

    parsed img html tag.

--- src/main/java/so/zjd/sstk/MobiCreator.java
+++ src/main/java/so/zjd/sstk/MobiCreator.java
@@ -7,6 +7,9 @@ import java.util.concurrent.Executors;
 
 public class MobiCreator {
 
+	private static final char[] IMG_START_TAG = new char[] { '<', 'i', 'm', 'g' };
+	private static final char[] IMG_END_TAG = new char[] { '/', '>' };
+
 	private static ExecutorService imgDownloaders = Executors.newFixedThreadPool(2);
 	private static List<String> supportedImgFormats = new ArrayList<String>();
 
@@ -18,12 +21,59 @@ public class MobiCreator {
 		supportedImgFormats.add(".BMP");
 	}
 	private PageEntry page;
+	private int index = 0;
 
 	public MobiCreator(PageEntry page) {
 		this.page = page;
 	}
 
 	public void create() {
-		System.out.println(page.getContent().toString().length());
+		processImages(page);
 	}
+
+	protected void processImages(PageEntry page) {
+		StringBuilder processed = new StringBuilder();
+		StringBuilder imgElement = new StringBuilder();
+		StringBuilder content = page.getContent();
+		// content = new
+		// StringBuilder("<p><span><img style=\"border: 1px solid black;\" src=\"http://images.cnitblog.com/blog/379997/201308/03231707-438355177e8940849c93a76bcff24ece.png\" alt=\"\" /></span></p>");
+		for (int i = 0; i < content.length(); i++) {
+			char c = content.charAt(i);
+			if (index < 4 && c == IMG_START_TAG[index]) {
+				index++;
+			} else {
+				index = 0;
+			}
+			if (index == 4) {
+				processed.delete(processed.length() - 3, processed.length());
+				imgElement.append("<img");
+				index = 0;
+				while (true) {
+					c = content.charAt(++i);
+					imgElement.append(c);
+					if (index < 2 && c == IMG_END_TAG[index]) {
+						index++;
+					} else {
+						index = 0;
+					}
+					if (index == 2) {
+						index = 0;
+						break;
+					}
+				}
+				System.out.println("img element:" + imgElement.toString());
+				downloadImg(imgElement);
+				processed.append(imgElement.toString());
+				imgElement.delete(0, imgElement.length());
+			} else {
+				processed.append(c);
+			}
+		}
+		System.out.println(processed.toString().equals(content.toString()));
+		System.out.println();
+	}
+	
+	private void downloadImg(StringBuilder imgElement){
+	}
+
 }
